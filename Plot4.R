b2ab230ee1f091e3da6d81ee19cd43db9132e278
tanayabedekar <38592949+tanayabedekar@users.noreply.github.com>
2018-06-04 17:45:28 -0700

    Add files via upload

---
+++ Plot4.R
@@ -0,0 +1,20 @@
+## Generating Plot 4
+par(mfrow = c(2,2), mar = c(4,4,2,1), oma = c(0,0,2,0))
+with(hpc1, {
+  plot(Global_active_power ~ Datetime, type = "l", 
+       ylab = "Global Active Power", xlab = "")
+  plot(Voltage ~ Datetime, type = "l", ylab = "Voltage", xlab = "datetime")
+  plot(Sub_metering_1 ~ Datetime, type = "l", ylab = "Energy sub metering",
+       xlab = "")
+  lines(Sub_metering_2 ~ Datetime, col = 'Red')
+  lines(Sub_metering_3 ~ Datetime, col = 'Blue')
+  legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 1, 
+         bty = "n",
+         legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power ~ Datetime, type = "l", 
+       ylab = "Global_rective_power", xlab = "datetime")
+})
+
+## Save file and close device
+dev.copy(png,"plot4.png", width=480, height=480)
+dev.off() 
